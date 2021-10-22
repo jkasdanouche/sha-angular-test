0c837e4f2acc6b5eba995feff8644f97a4516170
Jorge Kas-danouche <jkasdanouche@gmail.com>
2021-10-21 20:49:48 -0400

    configureing routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainPortalComponent } from './main-portal/main-portal.component';
 
 const routes: Routes = [
-  
+  { path: '', redirectTo: '/main', pathMatch: 'full'},
+  { path: 'main', component: MainPortalComponent }
 ];
 
 @NgModule({
